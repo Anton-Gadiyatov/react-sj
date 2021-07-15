a38712ed9330a67cfb56133f75bb8d438513f997
Anton Gadiyatov <gadiyatov.an@gmail.com>
2021-07-15 10:46:37 +0300

    feat: add basic socialnetwork layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './Header/Header'
-import Technologies from './Technologies/Technologies';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => {
   return (
-    <div>
+    <div className="app-wrapper">
       <Header />
-      <Technologies />
+      <Sidebar />
+      <main className="main">
+        <div>
+          <img
+            src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+            alt="bg-main"
+          ></img>
+        </div>
+        <div>
+          <img
+            src="https://lh3.googleusercontent.com/proxy/8j9HVXFNFrXaLDZNkafiHesyTrtuyKz4QPlbwfBjrU3pcZ7VIPWH72-DiSRysWA7TNIYhZPtpbtzAEoI0sZE6dpT"
+            alt="avatar"
+          ></img>
+        </div>
+        <div>ava + description</div>
+        <div>
+          My post
+          <div>New post</div>
+          <div>
+            <div>post 1</div>
+            <div>post 2</div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/v286batch2-aew-company-logo-22_3.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=311ef304bb090c4ba4f0ad95fad47fbf"
+        alt="header backgroundimage"
+      ></img>
+    </header>
+  );
+};
+
+export default Header;
