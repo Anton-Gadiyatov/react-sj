6d89e996fb7c52befc1f12d9dce04e77d85a3b6e
Anton Gadiyatov <gadiyatov.an@gmail.com>
2021-07-15 11:51:20 +0300

    feat: add Profile component

--- src/App.js
+++ src/App.js
@@ -2,35 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
+import Profile from "./components/Profile/Profile";
 
 const App = () => {
   return (
     <div className="app-wrapper">
       <Header />
       <Sidebar />
-      <main className="main">
-        <div>
-          <img
-            src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
-            alt="bg-main"
-          ></img>
-        </div>
-        <div>
-          <img
-            src="https://lh3.googleusercontent.com/proxy/8j9HVXFNFrXaLDZNkafiHesyTrtuyKz4QPlbwfBjrU3pcZ7VIPWH72-DiSRysWA7TNIYhZPtpbtzAEoI0sZE6dpT"
-            alt="avatar"
-          ></img>
-        </div>
-        <div>ava + description</div>
-        <div>
-          My post
-          <div>New post</div>
-          <div>
-            <div>post 1</div>
-            <div>post 2</div>
-          </div>
-        </div>
-      </main>
+      <Profile />
     </div>
   );
 };
