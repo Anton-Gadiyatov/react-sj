ea2fd719404fc2153de7dd7cc2f372db26d122de
Anton Gadiyatov <gadiyatov.an@gmail.com>
2021-07-17 11:03:58 +0300

    feat: add messages data variable

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -4,13 +4,21 @@ import styles from "./Dialogs.module.css";
 import Message from "./Message/Message";
 
 const dialogsData = [
-  { id: 1, name: 'Dimych'},
-  { id: 2, name: 'Andrey'},
-  { id: 3, name: 'Sveta'},
-  { id: 4, name: 'Sasha'},
-  { id: 5, name: 'Valera'},
-  { id: 6, name: 'Kostya'},
-]
+  { id: 1, name: "Dimych" },
+  { id: 2, name: "Andrey" },
+  { id: 3, name: "Sveta" },
+  { id: 4, name: "Sasha" },
+  { id: 5, name: "Valera" },
+  { id: 6, name: "Kostya" },
+];
+
+const messagesData = [
+  { id: 1, message: "Hi" },
+  { id: 2, message: "IT-Kamasutra" },
+  { id: 3, message: "Yo" },
+  { id: 4, message: "Yo" },
+  { id: 5, message: "Yo" },
+];
 
 const Dialogs = () => {
   return (
@@ -19,14 +27,11 @@ const Dialogs = () => {
         <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
         <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
         <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
-        <DialogItem name="Sasha" id="4" />
-        <DialogItem name="Valera" id="5" />
-        <DialogItem name="Kostya" id="6" />
       </div>
       <div className={styles.messages}>
-        <Message message="Hi" />
-        <Message message="Yo" />
-        <Message message="IT-Kamasutra" />
+        <Message id={messagesData[0].id} message={messagesData[0].message} />
+        <Message id={messagesData[1].id} message={messagesData[1].message} />
+        <Message id={messagesData[2].id} message={messagesData[2].message} />
       </div>
     </div>
   );
