2dd89e55de4957f69d1fb59ac56fe41b8a80f661
Anton Gadiyatov <gadiyatov.an@gmail.com>
2021-07-22 15:26:07 +0300

    feat: add store

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,59 +1,70 @@
-let rerenderEntireTree;
+const store = {
+  _state: {
+    dialogsPage: {
+      dialogsData: [
+        { id: 1, name: "Dimych" },
+        { id: 2, name: "Andrey" },
+        { id: 3, name: "Sveta" },
+        { id: 4, name: "Sasha" },
+        { id: 5, name: "Valera" },
+        { id: 6, name: "Kostya" },
+      ],
+      messagesData: [
+        { id: 1, message: "Hi" },
+        { id: 2, message: "IT-Kamasutra" },
+        { id: 3, message: "Yo" },
+        { id: 4, message: "Yo" },
+        { id: 5, message: "Yo" },
+      ],
+    },
+    profilePage: {
+      postsData: [
+        { id: 1, message: "hi, how are you?", likesCount: 10 },
+        { id: 2, message: "it's my first post", likesCount: 20 },
+      ],
+      newPostText: 'Kolumbus',
+    },
+    sidebar: {
+      friends: [
+        { id: 1, avatar: "Vasya", name: "Kloun" },
+        { id: 2, avatar: "Pasya", name: "Kloun" },
+        { id: 3, avatar: "Dasya", name: "Kloun" },
+      ],
+    },
+  },
 
-const state = {
-  dialogsPage: {
-    dialogsData: [
-      { id: 1, name: "Dimych" },
-      { id: 2, name: "Andrey" },
-      { id: 3, name: "Sveta" },
-      { id: 4, name: "Sasha" },
-      { id: 5, name: "Valera" },
-      { id: 6, name: "Kostya" },
-    ],
-    messagesData: [
-      { id: 1, message: "Hi" },
-      { id: 2, message: "IT-Kamasutra" },
-      { id: 3, message: "Yo" },
-      { id: 4, message: "Yo" },
-      { id: 5, message: "Yo" },
-    ],
+  getState() {
+    return this._state;
   },
-  profilePage: {
-    postsData: [
-      { id: 1, message: "hi, how are you?", likesCount: 10 },
-      { id: 2, message: "it's my first post", likesCount: 20 },
-    ],
-    newPostText: 'Kolumbus',
-  },
-  sidebar: {
-    friends: [
-      { id: 1, avatar: "Vasya", name: "Kloun" },
-      { id: 2, avatar: "Pasya", name: "Kloun" },
-      { id: 3, avatar: "Dasya", name: "Kloun" },
-    ],
-  },
-};
 
-export const updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
+  _callSubscriber() {
+    console.log('State changed');
+  },
+
+  addPost() {
+    const newPost = {
+      id: this._state.profilePage.postsData.length + 1,
+      message: this._state.profilePage.newPostText,
+      likesCount: 0,
+    };
+
+    if (this._state.profilePage.newPostText !== '') {
+      this._state.profilePage.postsData.push(newPost);
+      this._state.profilePage.newPostText = '';
+      this._callSubscriber(this._state);
+    }
+  },
+
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+
+  subscribe(observer) {
+    this._callSubscriber = observer;
+  }
+
 }
 
-
-export const addPost = () => {
-  const newPost = {
-    id: state.profilePage.postsData.length + 1,
-    message: state.profilePage.newPostText,
-    likesCount: 0,
-  };
-
-  state.profilePage.postsData.push(newPost);
-  state.profilePage.newPostText = '';
-  rerenderEntireTree(state);
-};
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
-}
-
-export default state;
+export default store;
+window.store = store;
