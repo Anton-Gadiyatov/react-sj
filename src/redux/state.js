6f9c0d862489597ef588115ef40e683e6673cfe9
Anton Gadiyatov <gadiyatov.an@gmail.com>
2021-07-22 17:13:32 +0300

    feat: add dispatch

--- src/redux/state.js
+++ src/redux/state.js
@@ -22,7 +22,7 @@ const store = {
         { id: 1, message: "hi, how are you?", likesCount: 10 },
         { id: 2, message: "it's my first post", likesCount: 20 },
       ],
-      newPostText: 'Kolumbus',
+      newPostText: "Kolumbus",
     },
     sidebar: {
       friends: [
@@ -33,38 +33,35 @@ const store = {
     },
   },
 
+  _callSubscriber() {
+    console.log("State changed");
+  },
+
   getState() {
     return this._state;
   },
-
-  _callSubscriber() {
-    console.log('State changed');
-  },
-
-  addPost() {
-    const newPost = {
-      id: this._state.profilePage.postsData.length + 1,
-      message: this._state.profilePage.newPostText,
-      likesCount: 0,
-    };
-
-    if (this._state.profilePage.newPostText !== '') {
-      this._state.profilePage.postsData.push(newPost);
-      this._state.profilePage.newPostText = '';
-      this._callSubscriber(this._state);
-    }
-  },
-
-  updateNewPostText(newText) {
-    this._state.profilePage.newPostText = newText;
-    this._callSubscriber(this._state);
-  },
-
   subscribe(observer) {
     this._callSubscriber = observer;
-  }
+  },
 
-}
+  dispatch(action) {
+    if (action.type === 'ADD-POST') {
+      const newPost = {
+        id: this._state.profilePage.postsData.length + 1,
+        message: this._state.profilePage.newPostText,
+        likesCount: 0,
+      };
+      if (this._state.profilePage.newPostText !== "") {
+        this._state.profilePage.postsData.push(newPost);
+        this._state.profilePage.newPostText = "";
+        this._callSubscriber(this._state);
+      }
+    } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+      this._state.profilePage.newPostText = action.newText;
+      this._callSubscriber(this._state);
+    }
+  }
+};
 
 export default store;
 window.store = store;
